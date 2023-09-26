f039b5558ac1c8fc80439f806b81c352708ec197
shaggy611 <sevczik11@gmail.com>
2023-09-26 05:29:20 +0300

    fix file links

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     build: {
         minify: false,
-        minifySyntax: false
+        minifySyntax: false,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                pages: resolve(__dirname, 'pages/superadmin_dashboard_1.html'),
+                pages_1: resolve(__dirname, 'pages/superadmin_dashboard_2.html'),
+            },
+        }
     }
 })
