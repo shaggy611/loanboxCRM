3e24bf97eca20aadd5e83f497ace74f5662b3da4
shaggy611 <sevczik11@gmail.com>
2023-09-28 18:13:54 +0300

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,10 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                pages: resolve(__dirname, 'pages/superadmin_dashboard_1.html'),
-                pages_1: resolve(__dirname, 'pages/superadmin_dashboard_2.html'),
+                pages: resolve(__dirname, '/pages/superadmin_dashboard_1.html'),
+                pages_1: resolve(__dirname, '/pages/superadmin_dashboard_2.html'),
+                pages_2: resolve(__dirname, '/pages/chief_collection_manager_dashboard_1.html'),
+                pages_3: resolve(__dirname, '/pages/chief_collection_manager_dashboard_2.html'),
             },
         }
     }
