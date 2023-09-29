904bc7c71dfcb4ddff1e7013c3bb505cbff73e57
shaggy611 <sevczik11@gmail.com>
2023-09-29 05:30:27 +0300

    add new page

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
                 pages_1: resolve(__dirname, 'pages/superadmin_dashboard_2.html'),
                 pages_2: resolve(__dirname, 'pages/chief_collection_manager_1.html'),
                 pages_3: resolve(__dirname, 'pages/chief_collection_manager_2.html'),
+                pages_4: resolve(__dirname, 'pages/lawyer_dashboard.html'),
+                pages_5: resolve(__dirname, 'pages/superadmin_dashboard_3.html')
             },
         }
     }
