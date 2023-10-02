1a9fcfca1ca0509c243bc493811d9b33fc0c8d7a
shaggy611 <sevczik11@gmail.com>
2023-10-02 12:42:37 +0300

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -21,8 +21,6 @@ export default defineConfig({
                 pages_10: resolve(__dirname, 'pages/login_page_3.html'),
                 pages_11: resolve(__dirname, 'pages/marketing_manager.html'),
             },
-        },
-        rollupOptions: {
             external: [
                 /^node:.*/,
             ]
