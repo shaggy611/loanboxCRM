dc268e6a6d6962b7cbf9b8eef73e47a87e7a5512
shaggy611 <sevczik11@gmail.com>
2023-10-02 14:34:59 +0300

    add new charts

--- main.js
+++ main.js
@@ -22,6 +22,11 @@ toggleButton.onblur = function () {
 
 // ======================================================
 const chartDonut = document.querySelector('#chart_donut')
+const linedChart = document.querySelector('#lined-chart')
+const cpaLinedChartPurple = document.querySelector('#cpa-lined-chart-purple')
+const cpaLinedChartBlue = document.querySelector('#cpa-lined-chart-blue')
+const responsesChartDonut = document.querySelector('#responses_chart_donut')
+const paymentsChartDonut = document.querySelector('#payments_chart_donut')
 
 document.addEventListener('DOMContentLoaded', () => {
     // Donut Chart
@@ -63,91 +68,93 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     // End Donut Chart
 
-    const a = Highcharts.chart('lined-chart', {
-        chart: {
-            type: 'spline',
-            scrollablePlotArea: {
-                minWidth: 600,
-                scrollPositionX: 1,
-            }
-        },
-        title: {
-            text: '',
-            align: 'left'
-        },
-        subtitle: {
-            text: '',
-            align: 'left'
-        },
-        xAxis: {
-            type: 'datetime',
-            labels: {
-                overflow: 'justify'
-            }
-        },
-        yAxis: {
+    if (linedChart) {
+        const a = Highcharts.chart('lined-chart', {
+            chart: {
+                type: 'spline',
+                scrollablePlotArea: {
+                    minWidth: 600,
+                    scrollPositionX: 1,
+                }
+            },
             title: {
-                text: ''
+                text: '',
+                align: 'left'
             },
-            minorGridLineWidth: 0,
-            gridLineWidth: 1,
-            alternateGridColor: '',
-
-        },
-        tooltip: {
-            valueSuffix: '',
-            crosshairs: true
-        },
-        plotOptions: {
-            spline: {
-                lineWidth: 2,
-                states: {
-                    hover: {
-                        lineWidth: 2
-                    }
+            subtitle: {
+                text: '',
+                align: 'left'
+            },
+            xAxis: {
+                type: 'datetime',
+                labels: {
+                    overflow: 'justify'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: ''
                 },
+                minorGridLineWidth: 0,
+                gridLineWidth: 1,
+                alternateGridColor: '',
+
+            },
+            tooltip: {
+                valueSuffix: '',
+                crosshairs: true
+            },
+            plotOptions: {
+                spline: {
+                    lineWidth: 2,
+                    states: {
+                        hover: {
+                            lineWidth: 2
+                        }
+                    },
+                    marker: {
+                        enabled: false
+                    },
+                    pointInterval: 3600000, // one hour
+                    pointStart: Date.UTC(2022, 5, 13, 0, 0, 0)
+                },
+            },
+            legend: {
+                align: 'left',
+                verticalAlign: 'top',
+                symbolWidth: 12,
+                margin: 30,
                 marker: {
-                    enabled: false
-                },
-                pointInterval: 3600000, // one hour
-                pointStart: Date.UTC(2022, 5, 13, 0, 0, 0)
+                    radius: 4,
+                    symbol: 'circle'
+                }
             },
-        },
-        legend: {
-            align: 'left',
-            verticalAlign: 'top',
-            symbolWidth: 12,
-            margin: 30,
-            marker: {
-                radius: 4,
-                symbol: 'circle'
-            }
-        },
-        series: [{
-            name: 'Payment',
-            lineWidth: 2,
-            data: [70, 67, 82, 28, 25, 62, 60, 87, 27, 80, 24, 26, 49, 68, 36, 22, 52, 90],
-            color: '#7367F0',
-            marker: {
-                radius: 4,
-                symbol: 'circle'
-            }
+            series: [{
+                name: 'Payment',
+                lineWidth: 2,
+                data: [70, 67, 82, 28, 25, 62, 60, 87, 27, 80, 24, 26, 49, 68, 36, 22, 52, 90],
+                color: '#7367F0',
+                marker: {
+                    radius: 4,
+                    symbol: 'circle'
+                }
 
-        }, {
-            name: 'Payout',
-            lineWidth: 2,
-            data: [73, 40, 41, 34, 77, 76, 52, 33, 67, 88, 68, 32, 22, 62, 25, 29, 45, 21],
-            color: 'rgba(254, 114, 8, 0.84)',
-            marker: {
-                radius: 4
+            }, {
+                name: 'Payout',
+                lineWidth: 2,
+                data: [73, 40, 41, 34, 77, 76, 52, 33, 67, 88, 68, 32, 22, 62, 25, 29, 45, 21],
+                color: 'rgba(254, 114, 8, 0.84)',
+                marker: {
+                    radius: 4
+                }
+            }],
+            navigation: {
+                menuItemStyle: {
+                    fontSize: '10px'
+                }
             }
-        }],
-        navigation: {
-            menuItemStyle: {
-                fontSize: '10px'
-            }
-        }
-    });
+        });
+    }
 
     // Progress chart
     // let chartProgress_1 = document.querySelector("#goals_chart_blue");
@@ -243,92 +250,273 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-    const b = Highcharts.chart('cpa-lined-chart-purple', {
-        chart: {
-            type: 'spline',
-            scrollablePlotArea: {
-                minWidth: 600,
-                scrollPositionX: 1,
-            }
-        },
-        title: {
-            text: '',
-            align: 'left'
-        },
-        subtitle: {
-            text: '',
-            align: 'left'
-        },
-        xAxis: {
-            type: 'datetime',
-            labels: {
-                overflow: 'justify'
-            }
-        },
-        yAxis: {
+    if (cpaLinedChartPurple) {
+        const b = Highcharts.chart('cpa-lined-chart-purple', {
+            chart: {
+                type: 'spline',
+                scrollablePlotArea: {
+                    minWidth: 600,
+                    scrollPositionX: 1,
+                }
+            },
             title: {
-                text: ''
+                text: '',
+                align: 'left'
             },
-            minorGridLineWidth: 0,
-            gridLineWidth: 1,
-            alternateGridColor: '',
-
-        },
-        tooltip: {
-            valueSuffix: '',
-            crosshairs: true
-        },
-        plotOptions: {
-            spline: {
-                lineWidth: 2,
-                states: {
-                    hover: {
-                        lineWidth: 2
-                    }
+            subtitle: {
+                text: '',
+                align: 'left'
+            },
+            xAxis: {
+                //categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                labels: {
+                    overflow: 'justify'
                 },
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                },
+                minorGridLineWidth: 0,
+                gridLineWidth: 1,
+                alternateGridColor: '',
+
+            },
+            tooltip: {
+                valueSuffix: '',
+                crosshairs: {
+                    width: 12,
+                    color: 'rgba(114, 103, 240, 0.299)',
+                }
+            },
+            plotOptions: {
+                spline: {
+                    lineWidth: 2,
+                    states: {
+                        hover: {
+                            lineWidth: 5
+                        }
+                    },
+                    marker: {
+                        enabled: false
+                    },
+                    pointInterval: 31 * 24 * 3600 * 1000,
+                    pointStart: Date.UTC(2022, 0, 1)
+                },
+                shadow: true,
+            },
+            legend: {
+                enabled: false,
+            },
+            series: [{
+                name: '',
+                lineWidth: 4,
+                data: [70, 67, 82, 28, 25, 62, 26, 49, 68, 36, 22, 52],
+                color: '#7367F0',
                 marker: {
-                    enabled: false
+                    radius: 0,
+                    symbol: 'circle'
                 },
-                pointInterval: 3600000, // one hour
-                pointStart: Date.UTC(2022, 5, 13, 0, 0, 0)
-            },
-        },
-        legend: {
-            align: 'left',
-            verticalAlign: 'top',
-            symbolWidth: 12,
-            margin: 30,
-            marker: {
-                radius: 4,
-                symbol: 'circle'
-            }
-        },
-        series: [{
-            name: 'Payment',
-            lineWidth: 2,
-            data: [70, 67, 82, 28, 25, 62, 60, 87, 27, 80, 24, 26, 49, 68, 36, 22, 52, 90],
-            color: '#7367F0',
-            marker: {
-                radius: 4,
-                symbol: 'circle'
-            }
+                shadow: {
+                    color: '#7367F0',
+                    width: 50,
+                    opacity: 2,
+                    offsetX: 0,
+                    offsetY: 40
+                }
 
-        }, {
-            name: 'Payout',
-            lineWidth: 2,
-            data: [73, 40, 41, 34, 77, 76, 52, 33, 67, 88, 68, 32, 22, 62, 25, 29, 45, 21],
-            color: 'rgba(254, 114, 8, 0.84)',
-            marker: {
-                radius: 4
+            }],
+            navigation: {
+                menuItemStyle: {
+                    fontSize: '12px'
+                }
             }
-        }],
-        navigation: {
-            menuItemStyle: {
-                fontSize: '10px'
+        });
+    }
+
+    if (cpaLinedChartBlue) {
+        const b = Highcharts.chart('cpa-lined-chart-blue', {
+            chart: {
+                type: 'spline',
+                scrollablePlotArea: {
+                    minWidth: 600,
+                    scrollPositionX: 1,
+                }
+            },
+            title: {
+                text: '',
+                align: 'left'
+            },
+            subtitle: {
+                text: '',
+                align: 'left'
+            },
+            xAxis: {
+                //categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                labels: {
+                    overflow: 'justify'
+                },
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                },
+                minorGridLineWidth: 0,
+                gridLineWidth: 1,
+                alternateGridColor: '',
+
+            },
+            tooltip: {
+                valueSuffix: '',
+                crosshairs: {
+                    width: 12,
+                    color: 'rgba(70, 127, 255, 0.24)',
+                }
+            },
+            plotOptions: {
+                spline: {
+                    lineWidth: 2,
+                    states: {
+                        hover: {
+                            lineWidth: 5
+                        }
+                    },
+                    marker: {
+                        enabled: false
+                    },
+                    pointInterval: 31 * 24 * 3600 * 1000,
+                    pointStart: Date.UTC(2022, 0, 1)
+                },
+                shadow: true,
+            },
+            legend: {
+                enabled: false,
+            },
+            series: [{
+                name: '',
+                lineWidth: 4,
+                data: [70, 67, 82, 28, 25, 62, 26, 49, 68, 36, 22, 52],
+                color: '#467FFF',
+                marker: {
+                    radius: 0,
+                    symbol: 'circle'
+                },
+                shadow: {
+                    color: '#467FFF',
+                    width: 50,
+                    opacity: 2,
+                    offsetX: 0,
+                    offsetY: 40
+                }
+
+            }],
+            navigation: {
+                menuItemStyle: {
+                    fontSize: '12px'
+                }
             }
-        }
-    });
+        });
+    }
+
+    if (responsesChartDonut) {
+        new Chart(
+            responsesChartDonut,
+
+            {
+                type: 'doughnut',
+                data: {
+                    labels: ['Item-1', 'Item-2', 'Item-3', 'Item-4', 'Item-5'],
+                    datasets: [
+                        {
+                            label: 'Loan body repayment',
+                            data: [1239132, 256413, 180345, 90345, 129825],
+                            backgroundColor: [
+                                'rgba(176, 169, 255, 0.49)'
+                            ],
+                            hoverOffset: 1,
+                            borderJoinStyle: 'round',
+                            borderWidth: 7,
+                            hoverBackgroundColor: '#7367F0',
+                            borderColor: 'white',
+                            borderRadius: 15,
+                            hoverBorderColor: 'transparent',
+                            rotation: -50
+                        }
+                    ]
+                },
+                options: {
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        tooltip: {
+                            usePointStyle: true,
+                            callbacks: {
+                                labelPointStyle: function (context) {
+                                    return {
+                                        pointStyle: 'dash',
+                                        rotation: 0,
+                                        width: 0
+                                    };
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        );
+    }
+
+    if (paymentsChartDonut) {
+        new Chart(
+            paymentsChartDonut,
+
+            {
+                type: 'doughnut',
+                data: {
+                    labels: ['No activity after completed loan 7 days', 'No activity after completed loan 7 days', 'No activity after completed loan 7 days', 'No activity after completed loan 7 days', 'No activity after completed loan 7 days'],
+                    datasets: [
+                        {
+                            labels: 'Payments',
+                            data: [1896, 256, 180, 903, 129],
+                            backgroundColor: [
+                                'rgba(70, 127, 255, 0.4528)'
+                            ],
+                            hoverOffset: 1,
+                            borderJoinStyle: 'round',
+                            borderWidth: 7,
+                            hoverBackgroundColor: '#467FFF',
+                            borderColor: 'white',
+                            borderRadius: 15,
+                            hoverBorderColor: 'transparent',
+                            rotation: -50
+                        }
+                    ]
+                },
+                options: {
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        tooltip: {
+                            usePointStyle: true,
+                            callbacks: {
+                                labelPointStyle: function (context) {
+                                    return {
+                                        pointStyle: 'dash',
+                                        rotation: 0,
+                                        width: 0
+                                    };
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        );
+    }
 
 })
 
